d7892b0418816bffd488614853f390f3a0a0acd6
andrei.avram <avram.andreimarius@gmail.com>
2020-06-27 21:50:35 +0300

    Added movement to sofa in living room.

--- story/Actions/WashHands.lua
+++ story/Actions/WashHands.lua
@@ -1,12 +1,12 @@
 WashHands = class(StoryActionBase, function(o, params)
-    StoryActionBase.init(o, " is washing hands ", params.performer, params.targetItem, params.nextLocation, params.prerequisites or {}, params.closingAction or nil, params.nextAction or nil)
+    StoryActionBase.init(o, " is washing hands in the", params.performer, params.targetItem, params.nextLocation, params.prerequisites or {}, params.closingAction or nil, params.nextAction or nil)
 end)
 
 function WashHands:Apply()
     local story = GetStory(self.Performer)
     table.insert(story.History, self)
     
-    story.Logger:Log(self.Performer:getData('skinDescription') .. self.Description .. " on the " .. self.TargetItem.Description, self.Performer)
+    story.Logger:Log(self.Performer:getData('skinDescription') .. self.Description .. self.TargetItem.Description, self.Performer)
     -- self.TargetItem.instance:setCollisionsEnabled(false)
 
     self.Performer:setAnimation("INT_HOUSE", "wash_up", 3000, true, true, false, true)
--- story/Episodes/House3.lua
+++ story/Episodes/House3.lua
@@ -29,7 +29,7 @@ function House3:Initialize(...)
     -- create two new chairs in the kitchen
     local bedroomChair1 = Chair{
         modelid =      Chair.eModel.SolidWoodenChair,
-        position =     Vector3(2494.2, -1708.3188, 1014.2422),
+        position =     Vector3(2494.5, -1708.3188, 1014.2422),
         rotation =     Vector3(0, 0, 0),
         noCollisions = true,
         interior =     self.InteriorId
@@ -39,7 +39,7 @@ function House3:Initialize(...)
 
     local bedroomChair2 = Chair{
         modelid =      Chair.eModel.SolidWoodenChair,
-        position =     Vector3(2494.2, -1706.7609, 1014.2422),
+        position =     Vector3(2494.5, -1706.7609, 1014.2422),
         rotation =     Vector3(0, 0, 0),
         noCollisions = true,
         interior =     self.InteriorId
@@ -51,9 +51,10 @@ function House3:Initialize(...)
     local kitchenSinkLocation = Location(2500.005859375, -1709.006225585938, 1014.7422, 270.000, self.InteriorId, "the sink in the kitchen")
     local kitchenFridgeLocation = Location(2498.2986, -1711.3533, 1014.7422, 169.6598, self.InteriorId, "fridge")
     local kitchenMicroWaveLocation = Location(2500.01416, -1711.3533, 1014.7422, 270.000, self.InteriorId, "microwave")
-    local kitchenChairLocation = Location(2494.9033, -1708.3198, 1014.7422, 90, self.InteriorId, "chair")
+    local kitchenChairLocation = Location(2495.2033, -1708.3198, 1014.7422, 90, self.InteriorId, "chair")
+    local livingroomSofaLocation = Location(2492.9772, -1698.654663085938, 1014.7422, 0, self.InteriorId, "chair")
 
-    table.insert(self.ValidStartingLocations, kitchenFridgeLocation)
+    table.insert(self.ValidStartingLocations, livingRoomEntranceLocation)
 
     -- Go to the sink in the kitchen
     table.insert(livingRoomEntranceLocation.PossibleActions, Move { performer = player, nextLocation = kitchenSinkLocation, targetItem = kitchenSinkLocation, graphId = self.graphId })
@@ -83,13 +84,24 @@ function House3:Initialize(...)
     -- cook food at the microwave
     local putInFoodAction = PutIn {performer = player, nextLocation = kitchenMicroWaveLocation, targetItem = food, where = "the microwave", graphId = self.graphId}
     table.insert(kitchenMicroWaveLocation.PossibleActions, putInFoodAction)
-    local waitAction = Wait { performer = player, nextLocation = kitchenMicroWaveLocation, time = 2000, graphId = self.graphId}
+    local waitAction = Wait { performer = player, nextLocation = kitchenMicroWaveLocation, time = 3000, graphId = self.graphId}
     putInFoodAction.NextAction = waitAction
     local pickUpFoodAction = PickUp {performer = player, nextLocation = kitchenMicroWaveLocation, targetItem = food, where = "the microwave", graphId = self.graphId}
     waitAction.NextAction = pickUpFoodAction
-    local moveToChairAction = Move { performer = player, nextLocation = kitchenChairLocation, targetItem = kitchenChairLocation, graphId = self.graphId}
-    pickUpFoodAction.NextAction = moveToChairAction
-    putInFoodAction.ClosingAction = moveToChairAction
+    local moveToKitchenChairAction = Move { performer = player, nextLocation = kitchenChairLocation, targetItem = kitchenChairLocation, graphId = self.graphId}
+    pickUpFoodAction.NextAction = moveToKitchenChairAction
+    putInFoodAction.ClosingAction = moveToKitchenChairAction
+
+    -- eat at the table
+    local sitDownKitchenChairAction = SitDown {performer = player, nextLocation = kitchenChairLocation, targetItem = bedroomChair1, graphId = self.graphId}
+    table.insert(kitchenChairLocation.PossibleActions, sitDownKitchenChairAction)
+    local eatKitchenChairAction = Eat {performer = player, nextLocation = kitchenChairLocation, targetItem = food, graphId = self.graphId}
+    sitDownKitchenChairAction.NextAction = eatKitchenChairAction
+    local standUpKitchenChiarAction = StandUp {performer = player, nextLocation = kitchenChairLocation, targetItem = bedroomChair1, graphId = self.graphId}
+    eatKitchenChairAction.NextAction = standUpKitchenChiarAction
+    local moveToLivingroomSofa = Move { performer = player, nextLocation = livingroomSofaLocation, targetItem = livingroomSofaLocation, graphId = self.graphId}
+    standUpKitchenChiarAction.NextAction = moveToLivingroomSofa
+    sitDownKitchenChairAction.ClosingAction = moveToLivingroomSofa
 
     if DEBUG then
         outputConsole("House3:Initialized")

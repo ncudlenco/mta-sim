3bb09d72b2b66fadfbf6bce3bd547a6daa6aa4a0
andrei.avram <avram.andreimarius@gmail.com>
2020-07-25 16:17:44 +0300

    Finished TurnTable actions.

---
+++ story/Actions/Dance.lua
@@ -0,0 +1,22 @@
+Dance = class(StoryActionBase, function(o, params)
+    StoryActionBase.init(o, " is dancing ", params.performer, params.targetItem, params.nextLocation, params.prerequisites or {}, params.closingAction or nil, params.nextAction or nil)
+end)
+
+function Dance:Apply()
+    local story = GetStory(self.Performer)
+    table.insert(story.History, self)
+    
+    story.Logger:Log(self.Performer:getData('skinDescription') .. self.Description, self.Performer)
+    -- self.TargetItem.instance:setCollisionsEnabled(false)
+
+    math.randomseed(os.time())
+    time = math.random(4000, 10000)
+
+    self.Performer:setAnimation("DANCING", "dance_loop", time, true, true, false, true)
+
+    if DEBUG then
+        outputConsole("Dance:Apply")
+    end
+
+    OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'))
+end

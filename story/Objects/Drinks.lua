fada58b95b5aa117d8a6b128b288f42bcabcf9d1
andrei.avram <avram.andreimarius@gmail.com>
2020-07-31 12:32:17 +0300

    Added more drinks.

--- story/Objects/Drinks.lua
+++ story/Objects/Drinks.lua
@@ -15,17 +15,40 @@ Drinks = class(SampStoryObjectBase, function(o, params)
         params.position.z = params.position.z + 0.15
     elseif params.modelid == Drinks.eModel.BottleCider1 or params.modelid == Drinks.eModel.BottleCider2 then
         params.position.z = params.position.z - 0.03
-    elseif params.modelid == Drinks.eModel.BottleWine then
+    elseif params.modelid == Drinks.eModel.BottleWine1 then
         params.position.z = params.position.z + 0.1
     elseif params.modelid == Drinks.eModel.GlassBeer then
         params.position.z = params.position.z + 0.05
-    elseif params.modelid == Drinks.eModel.GlassWine then
+    elseif params.modelid == Drinks.eModel.GlassAlcohol then
+        params.position.z = params.position.z + 0.05
+    elseif params.modelid == Drinks.eModel.GlassWine1 then
         params.position.z = params.position.z + 0.07
-    elseif params.modelid == Drinks.eModel.BottleVodka then
+    elseif params.modelid == Drinks.eModel.BottleVodka1 then
         params.position.z = params.position.z + 0.13
     elseif params.modelid == Drinks.eModel.BottleBeer1 or params.modelid == Drinks.eModel.BottleBeer2 then
         params.position.z = params.position.z + 0.1
         params.scale = 0.7
+    elseif params.modelid == Drinks.eModel.BottleWine2 then
+        params.position.z = params.position.z -0.02
+        params.scale = 0.7
+    elseif params.modelid == Drinks.eModel.BottleWine3 then
+        params.position.z = params.position.z -0.02
+        params.scale = 0.7
+    elseif params.modelid == Drinks.eModel.BottleVodka2 then
+        params.position.z = params.position.z -0.02
+        params.scale = 0.7
+    elseif params.modelid == Drinks.eModel.BottleAlcohol5 then
+        params.position.z = params.position.z -0.02
+        params.scale = 0.8
+    elseif params.modelid == Drinks.eModel.BottleWine4 then
+        params.position.z = params.position.z -0.03
+        params.scale = 0.7
+    elseif params.modelid == Drinks.eModel.GlassWine2 then
+        params.position.z = params.position.z + 0.03
+        params.scale = 0.8
+    elseif params.modelid == Drinks.eModel.GlassWine3 then
+        params.position.z = params.position.z + 0.03
+        params.scale = 0.8
     end
 
     SampStoryObjectBase.init(o, params)
@@ -50,17 +73,21 @@ function Drinks:updateDescription()
     elseif self.modelid == Drinks.eModel.MilkBottle then
         self.Description = "milk from a bottle"
     elseif self.modelid == Drinks.eModel.BottleAlcohol1 or self.modelid == Drinks.eModel.BottleAlcohol2 or 
-           self.modelid == Drinks.eModel.BottleAlcohol3 or self.modelid == Drinks.eModel.BottleAlcohol4 then
+           self.modelid == Drinks.eModel.BottleAlcohol3 or self.modelid == Drinks.eModel.BottleAlcohol4 or
+           self.modelid == Drinks.eModel.BottleAlcohol5 then
         self.Description = "alcohol from a bottle"
     elseif self.modelid == Drinks.eModel.BottleCider1 or self.modelid == Drinks.eModel.BottleCider2 then
         self.Description = "cider from a bottle"
-    elseif self.modelid == Drinks.eModel.BottleWine then
+    elseif self.modelid == Drinks.eModel.BottleWine1 or self.modelid == Drinks.eModel.BottleWine2 or 
+           self.modelid == Drinks.eModel.BottleWine3 or self.modelid == Drinks.eModel.BottleWine4 then
         self.Description = "wine from a bottle"
     elseif self.modelid == Drinks.eModel.GlassBeer then
         self.Description = "beer from a glass"
-    elseif self.modelid == Drinks.eModel.GlassWine then
+    elseif self.modelid == Drinks.eModel.GlassAlcohol then
+        self.Description = "alcohol from a glass"
+    elseif self.modelid == Drinks.eModel.GlassWine1 or self.modelid == Drinks.eModel.GlassWine2 or self.modelid == Drinks.eModel.GlassWine3 then
         self.Description = "wine from a glass"
-    elseif self.modelid == Drinks.eModel.BottleVodka then
+    elseif self.modelid == Drinks.eModel.BottleVodka1 or self.modelid == Drinks.eModel.BottleVodka2 then
         self.Description = "vodka from a bottle"
     elseif self.modelid == Drinks.eModel.BottleBeer1 or self.modelid == Drinks.eModel.BottleBeer2 then
         self.Description = "beer from a bottle"
@@ -88,14 +115,30 @@ function Drinks:updatePositionOffset()
         self.PosOffset = Vector3(-0.25, 0.03, 0.08)
     elseif self.modelid == Drinks.eModel.SodaCup2 then
         self.PosOffset = Vector3(0, 0.07, 0.09)
-    elseif self.modelid == Drinks.eModel.BottleWine then
+    elseif self.modelid == Drinks.eModel.BottleWine1 then
         self.PosOffset = Vector3(-0.05, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.BottleWine2 then
+        self.PosOffset = Vector3(-0.32, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.BottleWine3 then
+        self.PosOffset = Vector3(-0.26, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.BottleWine4 then
+        self.PosOffset = Vector3(-0.26, 0.03, 0.08)
     elseif self.modelid == Drinks.eModel.GlassBeer then
         self.PosOffset = Vector3(0, 0.03, 0.08)
-    elseif self.modelid == Drinks.eModel.GlassWine then
+    elseif self.modelid == Drinks.eModel.GlassAlcohol then
         self.PosOffset = Vector3(0, 0.03, 0.08)
-    elseif self.modelid == Drinks.eModel.BottleVodka then
+    elseif self.modelid == Drinks.eModel.GlassWine1 then
+        self.PosOffset = Vector3(0, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.GlassWine2 then
+        self.PosOffset = Vector3(-0.1, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.GlassWine3 then
+        self.PosOffset = Vector3(-0.05, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.BottleVodka1 then
         self.PosOffset = Vector3(-0.08, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.BottleVodka2 then
+        self.PosOffset = Vector3(-0.32, 0.03, 0.08)
+    elseif self.modelid == Drinks.eModel.BottleAlcohol5 then
+        self.PosOffset = Vector3(-0.20, 0.03, 0.08)
     elseif self.modelid == Drinks.eModel.BottleBeer1 or self.modelid == Drinks.eModel.BottleBeer2 then
         self.PosOffset = Vector3(-0.03, 0.03, 0.08)
     end
@@ -132,12 +175,20 @@ Drinks.eModel =
     BottleAlcohol2 = 1512,
     BottleAlcohol3 = 1517,
     BottleAlcohol4 = 1520,
+    BottleAlcohol5 = 1247,
     BottleCider1 = 1543,
     BottleCider2 = 1544,
-    BottleWine = 1664,
+    BottleWine1 = 1664,
+    BottleWine2 = 1239,
+    BottleWine3 = 1241,
+    BottleWine4 = 1254,
     GlassBeer = 1666,
-    GlassWine = 1667,
-    BottleVodka = 1668,
+    GlassAlcohol = 1455,
+    GlassWine1 = 1667,
+    GlassWine2 = 1253,
+    GlassWine3 = 1248,
+    BottleVodka1 = 1668,
+    BottleVodka2 = 1240,
     BottleBeer1 = 1950,
-    BottleBeer2 = 1951
+    BottleBeer2 = 1951,
 }

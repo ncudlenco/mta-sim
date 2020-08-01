d9d4a297c1569db3c1db9c389a5457ec23f67b70
andrei.avram <avram.andreimarius@gmail.com>
2020-08-01 11:37:01 +0300

    Modified video gen script.

--- scripts/images2video.py
+++ scripts/images2video.py
@@ -1,17 +1,26 @@
 import cv2
 import os
 
-image_folder = 'directory'
-video_name = 'video.avi'
+path = os.path.join("..", "data")
 
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+for directory in os.listdir(path):
+    print("Working on {}...".format(directory))
+    for subdirectory in os.listdir(os.path.join(path, directory)):
+        image_folder = os.path.join(path, directory, subdirectory)
+        video_name = os.path.join(path, directory, subdirectory, "video.avi")
+        images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+        frame = cv2.imread(os.path.join(image_folder, images[0]))
+        height, width, layers = frame.shape
+        height = int(height/4)
+        width = int(width/4)
+        frame = cv2.resize(frame, (width, height))
 
-video = cv2.VideoWriter(video_name, 0, 20, (width,height))
+        video = cv2.VideoWriter(video_name, 0, 10, (width,height))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
+        for image in images:
+            frame = cv2.imread(os.path.join(image_folder, image))
+            frame = cv2.resize(frame, (width, height))
+            video.write(frame)
 
-cv2.destroyAllWindows()
-video.release()
+        cv2.destroyAllWindows()
+        video.release()

7c5f4d0ff79012fb23dab3e18f163e56642b3457
andrei.avram <avram.andreimarius@gmail.com>
2020-08-15 17:39:50 +0300

    added better language description.

--- story/Logger.lua
+++ story/Logger.lua
@@ -2,6 +2,9 @@ Logger = class(StoryTextLoggerBase, function(o, path, showOnScreen, story)
     StoryTextLoggerBase.init(o, path)
     o.ShowOnScreen = showOnScreen
     o.Story = story
+    o.PhraseLinks = {". Then", ". After", ". Afterwards"}
+    o.FirstPhrase = true
+    o.PreviousAnd = true
 end)
 
 function Logger:GetElapsedTime()
@@ -18,12 +21,39 @@ function Logger:GetElapsedTime()
 end
 
 function Logger:Log(text, ...)
-    local logText = self:GetElapsedTime().." "..text
+    local player = nil
+    for i,v in ipairs(arg) do
+        player = v
+        break
+    end
+
+    if TIME_STAMP then
+        local logText = self:GetElapsedTime().." "..text.."\n" -- with stamp
+    else
+        if self.FirstPhrase then -- if its the frist phrase add the skin description
+            logText = text
+            self.FirstPhrase = false
+
+        else
+            math.randomseed(os.time())
+            dice = math.random()
+            
+            if dice > 0.7 and self.PreviousAnd == false then
+                logText = " and" .. string.sub(text, string.len(player:getData('skinDescription')) + 1) .. ". "
+                self.PreviousAnd = true
+            else
+                phraseLink = PickRandom(self.PhraseLinks)
+                logText = phraseLink .. " " .. player:getData('gender') .. string.sub(text, string.len(player:getData('skinDescription')) + 1)
+                self.PreviousAnd = false
+            end
+        end
+    end
+
     if LOG_DATA then
         local file = File(self.Path)
         if file then                               -- check if it was successfully opened
             file:setPos(file:getSize())            -- move position to the end of the file
-            file:write(logText .."\n")                      -- append data
+            file:write(logText)                    -- append data
             file:flush()                           -- Flush the appended data into the file.
             file:close()                           -- close the file once we're done with it
         else
@@ -31,11 +61,6 @@ function Logger:Log(text, ...)
         end
     end
     if self.ShowOnScreen then
-        local player = nil
-        for i,v in ipairs(arg) do
-            player = v
-            break
-        end
         if player then
             if player.outputChat then
                 player:outputChat(logText, 255, 0, 0, false)

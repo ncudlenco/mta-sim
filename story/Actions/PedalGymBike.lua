179bb82e565430b9d2a0177a97b7ba3bf307ddf7
andrei.avram <avram.andreimarius@gmail.com>
2020-09-10 19:41:27 +0300

    Added gymbike pedal actions.

---
+++ story/Actions/PedalGymBike.lua
@@ -0,0 +1,48 @@
+PedalGymBike = class(StoryActionBase, function(o, params)
+    StoryActionBase.init(o, " pedals ", params.performer, params.targetItem, params.nextLocation, params.prerequisites or {}, params.closingAction or nil, params.nextAction or nil)
+    o.how = params.how or PedalGymBike.eHow.Normal
+end)
+
+function PedalGymBike:Apply()
+    local story = GetStory(self.Performer)
+    table.insert(story.History, self)
+
+    if self.how == PedalGymBike.eHow.Still then
+        self.Description = " stands still on "
+    elseif self.how == PedalGymBike.eHow.Slow then
+        self.Description = " pedals slowly "
+    elseif self.how == PedalGymBike.eHow.Normal then
+        self.Description = " pedals "
+    elseif self.how == PedalGymBike.eHow.Fast then
+        self.Description = " pedals fast "
+    elseif self.how == PedalGymBike.eHow.Faster then
+        self.Description = " pedals very fast "
+    end
+        
+    story.Logger:Log(self.Performer:getData('skinDescription') .. self.Description .. self.TargetItem.Description, self.Performer)
+
+    math.randomseed(os.time())
+    local time = math.random(8000, 15000)
+    self.Performer:setAnimation("GYMNASIUM", self.how, time, true, true, false, true)
+
+    if DEBUG then
+        outputConsole("PedalGymBike:Apply")
+    end
+
+    OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'), function()
+        detachElementFromBone(self.TargetItem.instance)
+        self.TargetItem:Destroy()
+    end)
+end
+
+function PedalGymBike:GetDynamicString()
+    return 'return PedalGymBike{}'
+end
+
+PedalGymBike.eHow = {
+    Still = "gym_bike_slow",
+    Slow = "gym_bike_slow",
+    Normal = "gym_bike_pedal",
+    Fast = "gym_bike_fast",
+    Faster = "gym_bike_faster"
+}

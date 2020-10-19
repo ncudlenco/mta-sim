94067cc198a90dabb56da45610c7d25ae4f36da8
Nicolae Cudlenco <nicolae.cudlenco@cti.pub.ro>
2020-10-19 21:32:00 +0300

    Gym

--- story/Objects/Treadmill.lua
+++ story/Objects/Treadmill.lua
@@ -1,11 +1,11 @@
-TreadMill = class(SampStoryObjectBase, function(o, params)
+Treadmill = class(SampStoryObjectBase, function(o, params)
     params.description = "treadmill"
-    params.type = 'TreadMill'
+    params.type = 'Treadmill'
 
     SampStoryObjectBase.init(o, params)
 end
 )
 
-TreadMill.eModel = {
-    TreadMill1 = 2627
+Treadmill.eModel = {
+    Treadmill1 = 2627
 }

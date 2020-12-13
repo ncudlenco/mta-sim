b200f030e9cf4da99f8d70ceda5a83801de76100
Andrei-Marius Avram <avram.andreimarius@gmail.com>
2020-12-13 21:29:15 +0200

    Added paintings

---
+++ story/Objects/Painting.lua
@@ -0,0 +1,42 @@
+Painting = class(SampStoryObjectBase, function(o, params)
+    params.description = "painting"
+    params.type = 'Painting'
+
+    SampStoryObjectBase.init(o, params)
+end
+)
+
+Painting.eModel = 
+{
+    Painting01 = 2273,
+    Painting02 = 2265,
+    Painting03 = 2276,
+    Painting04 = 2282,
+    Painting05 = 2277,
+    Painting06 = 2274,
+    Painting07 = 2280,
+    Painting09 = 2275,
+    Painting10 = 2278,
+    Painting11 = 2276,
+    Painting12 = 2279,
+    Painting13 = 2281,
+    Painting14 = 2272,
+    Painting16 = 2284,
+    Painting17 = 2285,
+    Painting18 = 2286,
+    Painting19 = 2287,
+    Painting20 = 2288,
+    Painting21 = 2271,
+    Painting22 = 2261,
+    Painting24 = 2258,
+    Painting25 = 2259,
+    Painting26 = 2260,
+    Painting28 = 2262,
+    Painting29 = 2270,
+    Painting30 = 2263,
+    Painting31 = 2264,
+    Painting32 = 2265,
+    Painting33 = 2266,
+    Painting35 = 2268,
+    Painting36 = 2269,
+}

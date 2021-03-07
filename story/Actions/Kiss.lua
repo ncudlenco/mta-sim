1729702bbc18fa0bfcdcd1d81fad832f8ffa31e1
Andrei-Marius Avram <avram.andreimarius@gmail.com>
2021-03-07 10:39:58 +0200

    Improved multi agent actions

---
+++ story/Actions/Kiss.lua
@@ -0,0 +1,42 @@
+Kiss = class(StoryActionBase, function(o, params)
+    params.description = " kiss each other "
+    StoryActionBase.init(o,params)
+
+    o.TargetPlayer = params.targetPlayer
+end)
+
+function Kiss:Apply()
+    local story = GetStory(self.Performer)
+    table.insert(story.History[self.Performer:getData('id')], self)
+    
+    local time = 4000
+    story.Logger:Log(" and " .. self.TargetPlayer:getData('name') .. self.Description, self)
+
+    local performerKissType = nil
+    local targetKissType = nil
+
+    if self.Performer:getData('genderNominative') == "he" then
+        performerKissType = PickRandom({"playa_kiss_01", "playa_kiss_02", "playa_kiss_03"})
+    else
+        performerKissType = PickRandom({"grlfrd_kiss_01", "grlfrd_kiss_02", "grlfrd_kiss_03"})
+    end
+
+    if self.TargetPlayer:getData('genderNominative') == "she" then
+        targetKissType = PickRandom({"playa_kiss_01", "playa_kiss_02", "playa_kiss_03"})
+    else
+        targetKissType = PickRandom({"grlfrd_kiss_01", "grlfrd_kiss_02", "grlfrd_kiss_03"})
+    end
+
+    self.Performer:setAnimation("kissing", performerKissType, time, true, false, false, false)
+    self.TargetPlayer:setAnimation("kissing", targetKissType, time, true, false, false, false)
+
+    if DEBUG then
+        outputConsole("Kiss:Apply")
+    end
+
+    OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'))
+end
+
+function Kiss:GetDynamicString()
+    return 'return Kiss{}'
+end

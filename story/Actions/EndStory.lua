f007f1daec95d2a8ac06cd79adb54c426f4f82de
Nicolae Cudlenco <nicolae.cudlenco@cti.pub.ro>
2025-02-21 11:39:06 +0100

    feat: enhance story ending logic with performer pause and animation execution

--- story/Actions/EndStory.lua
+++ story/Actions/EndStory.lua
@@ -7,10 +7,18 @@ end)
 
 function EndStory:Apply()
     if DEBUG then
-        outputConsole("EndStory:Apply")
+        print("EndStory:Apply "..self.Performer:getData('id'))
     end
-    self.Performer:setAnimation()
+    if self.Performer:getData('storyEnded') then
+        return
+    end
+    local areOtherActorsStillPerforming = self:PausePerformer()
+    if not areOtherActorsStillPerforming then
+        self:EndStory()
+    end
+end
 
+function EndStory:EndStory()
     Timer(function(self)
         local story = GetStory(self.Performer)
         story:End()
@@ -21,6 +29,34 @@ function EndStory:Apply()
             end
         end
     end, 5000, 1, self)
+end
+
+function EndStory:PausePerformer()
+    local player = self.Performer
+    local otherActorsStillPerforming = false
+    if LOAD_FROM_GRAPH then
+        if Any(CURRENT_STORY.CurrentEpisode.peds, function(ped) return player:getData('id') ~= ped:getData('id') and not ped:getData('storyEnded') end) then
+            if DEBUG then
+                print("EndStory:PausePerformer - waiting for the others to finish")
+            end
+            print("EndStory:PausePerformer - actor "..player:getData('id').." finished and is waiting for the others to finish")
+            local unfinishedActors = Where(CURRENT_STORY.CurrentEpisode.peds, function(ped) return player:getData('id') ~= ped:getData('id') and not ped:getData('storyEnded') end)
+            for i,p in ipairs(unfinishedActors) do
+                print(p:getData('id'))
+            end
+            --the episode is ended for the current actor, wait for all the other actors to finish
+            otherActorsStillPerforming = true
+        end
+    end
+
+    player:setData('storyEnded', true)
+    self:ExecuteEndAnimations()
+
+    return otherActorsStillPerforming
+end
+
+function EndStory:ExecuteEndAnimations()
+    self.Performer:setAnimation("cop_ambient", "coplook_loop", 0, true, false, false, true)
 end
 
 function EndStory:GetDynamicString()

3cf7a2eb27a4e082e275ec7c7f8e8f1b83dd6d03
Nicolae Cudlenco <nicolae.cudlenco@cti.pub.ro>
2025-10-28 11:40:39 +0100

    feat: stabilize interactions

--- src/story/Actions/Give.lua
+++ src/story/Actions/Give.lua
@@ -1,75 +1,73 @@
-Give = class(StoryActionBase, function(o, params)
+Give = class(InteractionActionBase, function(o, params)
     params.description = " gives "
     params.name = 'Give'
+    params.interactionOffset = Vector3(-0.5, -0.5, 0)
 
-    StoryActionBase.init(o,params)
+    InteractionActionBase.init(o, params)
 
-    o.TargetPlayer = params.targetPlayer
     o.how = params.how or PickUp.eHow.Normal
     o.hand = params.hand or PickUp.eHand.Right
-    o.isInteraction = true
 end)
 
 function Give:Apply()
     local story = GetStory(self.Performer)
     table.insert(story.History[self.Performer:getData('id')], self)
-
     StoryActionBase.Apply(self)
 
-    local function faceP1ToP2(p1, p2)
-        local targetFront = p2.position - p1.position
-        local angle = p1.matrix.forward:angleAboutAxis(targetFront, p1.matrix.up)
-        p1.rotation = Vector3(0,0,p1.rotation.z + math.deg(angle))
-    end
-
-    self.TargetPlayer.position = self.Performer.position + Vector3(-0.5,-0.5,0)
-
-    outputConsole("Facing players one to the other...")
-    faceP1ToP2(self.Performer, self.TargetPlayer)
-    faceP1ToP2(self.TargetPlayer, self.Performer)
-
     local time = 1000
-    StoryActionBase.GetLogger(self, story):Log(" and " .. self.TargetPlayer:getData('name') .. self.Description, self)
 
-    outputConsole("Playing hanshake animation...")
-    self.Performer:setAnimation("gangs", "hndshkfa_swt", time, true, false, false, false)
-    self.TargetPlayer:setAnimation("gangs", "hndshkfa_swt", time, true, false, false, false)
+    -- Synchronize actors: rotation → delay → position check → animations
+    self:SyncActors(
+        self.Performer,
+        self.TargetPlayer,
+        self.InteractionOffset,
+        function()
+            -- Actors are now properly positioned and facing each other
+            StoryActionBase.GetLogger(self, story):Log(" and " .. self.TargetPlayer:getData('name') .. self.Description, self)
 
-    if DEBUG then
-        outputConsole("Give:Apply")
-    end
+            outputConsole("Playing handshake animation...")
+            self.Performer:setAnimation("gangs", "hndshkfa_swt", time, true, false, false, false)
+            self.TargetPlayer:setAnimation("gangs", "hndshkfa_swt", time, true, false, false, false)
 
-    outputConsole("Swapping object from one player to the other...")
-    OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'), function()
-        detachElementFromBone(self.TargetItem.instance)
-        local pickedObjects = self.Performer:getData('pickedObjects')
-        if type(pickedObjects) == 'boolean' then
-            pickedObjects = {}
+            if DEBUG then
+                outputConsole("Give:Apply")
+            end
+
+            outputConsole("Swapping object from one player to the other...")
+            -- Schedule object transfer from performer to target
+            OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'), function()
+                detachElementFromBone(self.TargetItem.instance)
+                local pickedObjects = self.Performer:getData('pickedObjects')
+                if type(pickedObjects) == 'boolean' then
+                    pickedObjects = {}
+                end
+                pickedObjects = Where(pickedObjects, function(po) return po[1] ~= self.TargetItem.ObjectId end)
+                self.Performer:setData('pickedObjects', pickedObjects)
+            end)
+
+            OnGlobalActionFinished(time+10, self.TargetPlayer:getData('id'), self.TargetPlayer:getData('storyId'), function()
+                if self.how == PickUp.eHow.Normal then
+                    self.TargetItem:updatePositionOffsetStandUp()
+                    self.TargetItem:updateRotOffsetStandUp()
+                elseif self.how == PickUp.eHow.Sit then
+                    self.TargetItem:updatePositionOffsetSitDown()
+                    self.TargetItem:updateRotOffsetSitDown()
+                end
+
+                attachElementToBone(self.TargetItem.instance, self.TargetPlayer, self.hand,
+                    self.TargetItem.PosOffset.x, self.TargetItem.PosOffset.y, self.TargetItem.PosOffset.z,
+                    self.TargetItem.RotOffset.x, self.TargetItem.RotOffset.y, self.TargetItem.RotOffset.z
+                )
+
+                local pickedObjects = self.TargetPlayer:getData('pickedObjects')
+                if type(pickedObjects) == 'boolean' then
+                    pickedObjects = {}
+                end
+                table.insert(pickedObjects, {self.TargetItem.ObjectId, self.TargetItem.Description})
+                self.TargetPlayer:setData('pickedObjects', pickedObjects)
+            end)
         end
-        pickedObjects = Where(pickedObjects, function(po) return po[1] ~= self.TargetItem.ObjectId end)
-        self.Performer:setData('pickedObjects', pickedObjects)
-    end)
-    OnGlobalActionFinished(time+10, self.TargetPlayer:getData('id'), self.TargetPlayer:getData('storyId'), function()
-        if self.how == PickUp.eHow.Normal then
-            self.TargetItem:updatePositionOffsetStandUp()
-            self.TargetItem:updateRotOffsetStandUp()
-        elseif self.how == PickUp.eHow.Sit then
-            self.TargetItem:updatePositionOffsetSitDown()
-            self.TargetItem:updateRotOffsetSitDown()
-        end
-        attachElementToBone(self.TargetItem.instance, self.TargetPlayer, self.hand,
-            self.TargetItem.PosOffset.x, self.TargetItem.PosOffset.y, self.TargetItem.PosOffset.z,
-            self.TargetItem.RotOffset.x, self.TargetItem.RotOffset.y, self.TargetItem.RotOffset.z
-        )
-
-        local pickedObjects = self.TargetPlayer:getData('pickedObjects')
-
-        if type(pickedObjects) == 'boolean' then
-            pickedObjects = {}
-        end
-        table.insert(pickedObjects, {self.TargetItem.ObjectId, self.TargetItem.Description})
-        self.TargetPlayer:setData('pickedObjects', pickedObjects)
-    end)
+    )
 end
 
 function Give:GetDynamicString()

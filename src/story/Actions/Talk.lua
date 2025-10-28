c49ee1823165fc14dde4db693757c20c02b39fbb
Nicolae Cudlenco <nicolae.cudlenco@cti.pub.ro>
2025-10-28 12:01:36 +0100

    refactor: update Talk action to use InteractionActionBase and streamline synchronization

--- src/story/Actions/Talk.lua
+++ src/story/Actions/Talk.lua
@@ -1,47 +1,46 @@
-Talk = class(StoryActionBase, function(o, params)
+Talk = class(InteractionActionBase, function(o, params)
     params.description = " talks to "
     params.name = 'Talk'
+    params.interactionOffset = Vector3(-0.5, -0.5, 0)
 
-    StoryActionBase.init(o,params)
-
-    o.TargetPlayer = params.targetPlayer
-    o.isInteraction = true
+    InteractionActionBase.init(o, params)
 end)
 
 function Talk:Apply()
     local story = GetStory(self.Performer)
     table.insert(story.History[self.Performer:getData('id')], self)
-
     StoryActionBase.Apply(self)
 
-    local function faceP1ToP2(p1, p2)
-        local targetFront = p2.position - p1.position
-        local angle = p1.matrix.forward:angleAboutAxis(targetFront, p1.matrix.up)
-        p1.rotation = Vector3(0,0,p1.rotation.z + math.deg(angle))
-    end
-
-
-    faceP1ToP2(self.Performer, self.TargetPlayer)
-    faceP1ToP2(self.TargetPlayer, self.Performer)
     local time = math.random(2, 4) * 1000
 
-    StoryActionBase.GetLogger(self, story):Log(self.Description .. self.TargetPlayer:getData('name'), self)
-    local talkType = PickRandom({"prtial_gngtlka", "prtial_gngtlkb", "prtial_gngtlkc",
-                                 "prtial_gngtlkd", "prtial_gngtlke", "prtial_gngtlkf",
-                                 "prtial_gngtlkg", "prtial_gngtlkh"})
-    self.Performer:setAnimation("gangs", talkType, time, true, false, false, false)
-    talkType = PickRandom({"prtial_gngtlka", "prtial_gngtlkb", "prtial_gngtlkc",
-                                 "prtial_gngtlkd", "prtial_gngtlke", "prtial_gngtlkf",
-                                 "prtial_gngtlkg", "prtial_gngtlkh"})
-    self.TargetPlayer:setAnimation("gangs", talkType, time, true, false, false, false)
+    -- Synchronize actors: rotation → delay → position check → animations
+    self:SyncActors(
+        self.Performer,
+        self.TargetPlayer,
+        self.InteractionOffset,
+        function()
+            -- Actors are now properly positioned and facing each other
+            StoryActionBase.GetLogger(self, story):Log(self.Description .. self.TargetPlayer:getData('name'), self)
 
+            local talkType = PickRandom({"prtial_gngtlka", "prtial_gngtlkb", "prtial_gngtlkc",
+                                         "prtial_gngtlkd", "prtial_gngtlke", "prtial_gngtlkf",
+                                         "prtial_gngtlkg", "prtial_gngtlkh"})
+            self.Performer:setAnimation("gangs", talkType, time, true, false, false, false)
 
-    if DEBUG then
-        outputConsole("Talk:Apply")
-    end
+            talkType = PickRandom({"prtial_gngtlka", "prtial_gngtlkb", "prtial_gngtlkc",
+                                   "prtial_gngtlkd", "prtial_gngtlke", "prtial_gngtlkf",
+                                   "prtial_gngtlkg", "prtial_gngtlkh"})
+            self.TargetPlayer:setAnimation("gangs", talkType, time, true, false, false, false)
 
-    OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'))
-    OnGlobalActionFinished(time, self.TargetPlayer:getData('id'), self.TargetPlayer:getData('storyId'))
+            if DEBUG then
+                outputConsole("Talk:Apply")
+            end
+
+            -- Schedule action completion
+            OnGlobalActionFinished(time, self.Performer:getData('id'), self.Performer:getData('storyId'))
+            OnGlobalActionFinished(time, self.TargetPlayer:getData('id'), self.TargetPlayer:getData('storyId'))
+        end
+    )
 end
 
 function Talk:GetDynamicString()
